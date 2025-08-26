9227c4f8297332c8ed4d5540ed806cf36c07bb16
sthickpennyryan <162819635+sthickpennyryan@users.noreply.github.com>
2025-08-26 18:54:03 +0100

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -30,7 +30,8 @@ SELECT
     COUNT(*) 
 FROM silver.crm_cust_info
 GROUP BY cst_id
-HAVING COUNT(*) > 1 OR cst_id IS NULL;
+HAVING COUNT(*) > 1 
+OR cst_id IS NULL;
 
 -- Check for Unwanted Spaces
 -- Expectation: No Results
@@ -54,7 +55,8 @@ SELECT
     COUNT(*) 
 FROM silver.crm_prd_info
 GROUP BY prd_id
-HAVING COUNT(*) > 1 OR prd_id IS NULL;
+HAVING COUNT(*) > 1 
+OR prd_id IS NULL;
 
 -- Check for Unwanted Spaces
 -- Expectation: No Results
@@ -68,7 +70,8 @@ WHERE prd_nm != TRIM(prd_nm);
 SELECT 
     prd_cost 
 FROM silver.crm_prd_info
-WHERE prd_cost < 0 OR prd_cost IS NULL;
+WHERE prd_cost < 0 
+OR prd_cost IS NULL;
 
 -- Data Standardization & Consistency
 SELECT DISTINCT 
@@ -91,9 +94,9 @@ SELECT
     NULLIF(sls_due_dt, 0) AS sls_due_dt 
 FROM bronze.crm_sales_details
 WHERE sls_due_dt <= 0 
-    OR LEN(sls_due_dt) != 8 
-    OR sls_due_dt > 20500101 
-    OR sls_due_dt < 19000101;
+  OR LEN(sls_due_dt) != 8 
+  OR sls_due_dt > 20500101 
+  OR sls_due_dt < 19000101;
 
 -- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
 -- Expectation: No Results
@@ -101,7 +104,7 @@ SELECT
     * 
 FROM silver.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt 
-   OR sls_order_dt > sls_due_dt;
+OR sls_order_dt > sls_due_dt;
 
 -- Check Data Consistency: Sales = Quantity * Price
 -- Expectation: No Results
@@ -111,12 +114,12 @@ SELECT DISTINCT
     sls_price 
 FROM silver.crm_sales_details
 WHERE sls_sales != sls_quantity * sls_price
-   OR sls_sales IS NULL 
-   OR sls_quantity IS NULL 
-   OR sls_price IS NULL
-   OR sls_sales <= 0 
-   OR sls_quantity <= 0 
-   OR sls_price <= 0
+	OR sls_sales IS NULL 
+	OR sls_quantity IS NULL 
+	OR sls_price IS NULL
+	OR sls_sales <= 0 
+	OR sls_quantity <= 0 
+	OR sls_price <= 0
 ORDER BY sls_sales, sls_quantity, sls_price;
 
 -- ====================================================================
@@ -124,6 +127,7 @@ ORDER BY sls_sales, sls_quantity, sls_price;
 -- ====================================================================
 -- Identify Out-of-Range Dates
 -- Expectation: Birthdates between 1924-01-01 and Today
+
 SELECT DISTINCT 
     bdate 
 FROM silver.erp_cust_az12
@@ -160,5 +164,4 @@ WHERE cat != TRIM(cat)
 SELECT DISTINCT 
     maintenance 
 FROM silver.erp_px_cat_g1v2;
-
 
