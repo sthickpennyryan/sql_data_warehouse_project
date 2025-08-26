ae7b7707b39565e14734d37df11494065d9613ef
sthickpennyryan <162819635+sthickpennyryan@users.noreply.github.com>
2025-08-26 18:49:29 +0100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -11,6 +11,8 @@ Actions:
 
 IF OBJECT_ID('silver.crm_cust_info' , 'U') IS NOT NULL 
 	DROP TABLE silver.crm_cust_info;
+GO
+
 CREATE TABLE silver.crm_cust_info (
 	cst_id INT,
 	cst_key NVARCHAR(50),
@@ -21,9 +23,11 @@ CREATE TABLE silver.crm_cust_info (
 	cst_create_date DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
 
 IF OBJECT_ID('silver.crm_prd_info' , 'U') IS NOT NULL 
 	DROP TABLE silver.crm_prd_info;
+GO
 CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
 	cat_id NVARCHAR(50),
@@ -35,10 +39,12 @@ CREATE TABLE silver.crm_prd_info(
 	prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 
 IF OBJECT_ID('silver.crm_sales_details' , 'U') IS NOT NULL 
 	DROP TABLE silver.crm_sales_details;
+GO
+
 CREATE TABLE silver.crm_sales_details(
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
@@ -51,29 +57,33 @@ CREATE TABLE silver.crm_sales_details(
 	sls_price INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 
 IF OBJECT_ID('silver.erp_cust_az12' , 'U') IS NOT NULL 
 	DROP TABLE silver.erp_cust_az12;
+GO
 CREATE TABLE silver.erp_cust_az12(
 	cid NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 
 IF OBJECT_ID('silver.erp_loc_a101' , 'U') IS NOT NULL 
 	DROP TABLE silver.erp_loc_a101;
+GO
+
 CREATE TABLE silver.erp_loc_a101(
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2' , 'U') IS NOT NULL 
 	DROP TABLE silver.erp_px_cat_g1v2;
+GO
 CREATE TABLE silver.erp_px_cat_g1v2(
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
@@ -81,3 +91,4 @@ CREATE TABLE silver.erp_px_cat_g1v2(
 	maintenance NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+GO
